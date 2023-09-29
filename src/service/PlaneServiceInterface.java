72942f1dff21fe293b7d62778e75c33be934473e
Marv1996 <koryun.yenokyan@gmail.com>
2023-09-29 16:20:58 +0400

    Third commit

---
+++ src/service/PlaneServiceInterface.java
@@ -0,0 +1,26 @@
+package service;
+
+import model.Plane;
+
+public interface PlaneServiceInterface {
+
+    void Task1(Plane plane);
+
+    void Task2(Plane plane);
+
+    Plane Task3(Plane planeFirst, Plane planeSecond);
+
+    String Task4(Plane planeFirst, Plane planeSecond);
+
+    void Task5(Plane planeFirst, Plane planeSecond, Plane planeThird);
+
+    void Task6(Plane[] planes);
+
+    void Task7(Plane[] planes);
+
+    Plane Task8(Plane[] planes);
+
+    Plane Task9(Plane[] planes);
+
+    void Task10(Plane[] planes);
+}
